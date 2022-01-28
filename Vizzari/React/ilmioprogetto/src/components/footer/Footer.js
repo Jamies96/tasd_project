a51be6912842b04bf0089eafcf3248dd3c79d21f
Nada Cenci <nada.cenci@hotmail.com>
2022-01-28 19:15:09 +0100

    target + noreferrer

--- Vizzari/React/ilmioprogetto/src/components/footer/Footer.js
+++ Vizzari/React/ilmioprogetto/src/components/footer/Footer.js
@@ -19,7 +19,7 @@ function Footer(props) {
     const listItems = pageListItem.map((item) => {
         return (
             <li className="nav-item">
-                <NavLink to={item.url} target="_blank" rel="noreferrer" className="footer-link">
+                <NavLink to={item.url} rel="noreferrer" target="_blank" className="footer-link">
                     {item.text}
                 </NavLink>
             </li>
